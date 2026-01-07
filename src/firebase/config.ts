2adbda6c531a467231ad887f9660a088b7fac1b5
STUFFACTORY STF <stfshare01@gmail.com>
2026-01-07 22:58:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4943620722-1724d",
+  "appId": "1:724813140822:web:49e49aeb992f25a79cbcf7",
+  "apiKey": "AIzaSyClQHjxPoyI3ba5nxYOz_V1I416vmui7Ok",
+  "authDomain": "studio-4943620722-1724d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "724813140822"
+};
